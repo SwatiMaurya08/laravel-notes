bf24ae61c1b9300eaaf0de89070b0c387ea77c54
SwatiMaurya08 <swatimaurya704@gmail.com>
2020-07-05 13:34:15 +0530

    added laravel-rest

---
+++ laravel-rest/app/Http/Controllers/AuthController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function store(Request $request)
+    {
+
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:5'
+
+        ]);
+
+        $name = $request->input('name');
+        $email = $request->input('email');
+        $password = $request->input('password');
+
+        $user = [
+            'name' => $name,
+            'email' => $email,
+            'password' => $password,
+            'sigin' => [
+                'href' => 'api/v1/user/signin',
+                'method' => 'POST',
+                'param' => 'email , password'
+            ]
+
+        ];
+
+
+        $response = [
+            'msg' => 'User Created!!!',
+            'user' => $user,
+        ];
+        return response()->json($response, 201);
+    }
+
+    public function signin(Request $request)
+    {
+
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:5'
+
+        ]);
+        $email = $request->input('email');
+        $password = $request->input('password');
+        return "It works";
+    }
+
+}
